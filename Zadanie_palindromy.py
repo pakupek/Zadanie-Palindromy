c28d6f346163ffea30f181c57614ab9a119eb693
Adam Akimowicz <adam.akimowicz13@onet.eu>
2022-04-28 11:32:33 +0200

    Add documentary

--- Zadanie_palindromy.py
+++ Zadanie_palindromy.py
@@ -5,6 +5,13 @@
 #Pamiętaj, że string/tekst, to kolekcja znaków. Znasz już funkcje kolekcji, które pozwalają odnosić się do elementów indeksowanych od początku i od końca.
 '''
 def palindrome(word_to_be_checked):
+    """
+    Funkcja palindrome opiera sie na argumencie word_to_be_checked.
+    Następnie zamienia duże litery na małe które zostały podane w argumencie.
+    Jeżeli tego bym  nie zrobił to słowo Potop nie bedzie palindromem gdyż Potop != potoP.
+    Warunek if sprawdza czy podane słowo w argumencie bedzie takie samo po przejsciu od końca.
+    Jeżeli tak to wypisze na konsoli True, w przeciwnym wypadku False
+    """
     word_to_be_checked = word_to_be_checked.lower()
     if word_to_be_checked == word_to_be_checked[::-1]:
         print('True')
@@ -18,6 +25,15 @@ word_to_be_checked = 'Potop'
 w = ''
 
 for i in word_to_be_checked:
+    """
+    W tym sposobie wykorzystałem pętle for która przechodzi przez kolejne litery w zmiennej word_to_be_checked.
+    Tak samo jak w pierwszym rozwiązaniu zamieniam dużą litere na małą.
+    Każde przejście przez litere jest przypisywane do zmiennej 'w'która przechowa każdą litere ze zmiennej word_to_be_checked.
+    Nastepnie warunkiem if sprawdzam czy podane słowo w zmiennej word_to_be_checked jest równe w utworzonej zmiennej 'w'.
+    Jeżeli tak to wypisze na konsoli True, w przeciwnym wypadku False.
+    To mój taki dodatkowy sposób na rozwiązanie zadania nie wykorzystując funkcji.
+    Mam nadzieje że jest to w miare zrozumiałe :).
+    """
     word_to_be_checked = word_to_be_checked.lower()
     w = w.lower()
     w = i+w
