2a7f36224b4aa9ae37703bbb97233d2bcb917f6e
Adam Akimowicz <adam.akimowicz13@onet.eu>
2022-04-28 11:17:23 +0200

    Add solution nr 2

--- Zadanie_palindromy.py
+++ Zadanie_palindromy.py
@@ -3,7 +3,7 @@
 #Palindrom to słowo, które czytane od lewej do prawej i od prawej do lewej brzmi tak samo. Przykłady to “kajak” i “potop”.
 #Zaprogramuj funkcję, która przyjmuje jeden argument (typu string) i zwraca wartość boolean: True/False, mówiącą czy podany tekst jest palindromem.
 #Pamiętaj, że string/tekst, to kolekcja znaków. Znasz już funkcje kolekcji, które pozwalają odnosić się do elementów indeksowanych od początku i od końca.
-
+'''
 def palindrome(word_to_be_checked):
     word_to_be_checked = word_to_be_checked.lower()
     if word_to_be_checked == word_to_be_checked[::-1]:
@@ -12,3 +12,18 @@ def palindrome(word_to_be_checked):
         print('False')
 
 palindrome(word_to_be_checked = 'Potop')
+'''
+
+word_to_be_checked = 'Potop'
+w = ''
+
+for i in word_to_be_checked:
+    word_to_be_checked = word_to_be_checked.lower()
+    w = w.lower()
+    w = i+w
+if (word_to_be_checked == w):
+    print('True')
+else:
+    print('False')
+
+
