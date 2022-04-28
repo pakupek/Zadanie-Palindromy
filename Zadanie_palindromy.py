ea5b85c282de3053763823c9dec98203789757eb
Adam Akimowicz <adam.akimowicz13@onet.eu>
2022-04-28 11:11:41 +0200

    Add task content

---
+++ Zadanie_palindromy.py
@@ -0,0 +1,5 @@
+                                        #Zadanie: palindromy
+#Twoim zadaniem będzie napisanie funkcji, która sprawdza, czy dany wyraz jest palindromem. 
+#Palindrom to słowo, które czytane od lewej do prawej i od prawej do lewej brzmi tak samo. Przykłady to “kajak” i “potop”.
+#Zaprogramuj funkcję, która przyjmuje jeden argument (typu string) i zwraca wartość boolean: True/False, mówiącą czy podany tekst jest palindromem.
+#Pamiętaj, że string/tekst, to kolekcja znaków. Znasz już funkcje kolekcji, które pozwalają odnosić się do elementów indeksowanych od początku i od końca.
